2af429759b6244352a51e0054736aed2b2d50301
zeyad rashed <brwnboy2002@gmail.com>
2024-03-12 16:48:03 -0400

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # javafx-assignment
+
 21:198:102/02: Computers and Programming II Spring 2024
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module xyz.zuner.javafxassignment {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens xyz.zuner.javafxassignment to javafx.fxml;
+    exports xyz.zuner.javafxassignment;
+}
