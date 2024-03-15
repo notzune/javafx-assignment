3b358e7e5067b02f856bcc3fbd804a287d3054b9
zeyad rashed <brwnboy2002@gmail.com>
2024-03-14 22:02:52 -0400

    feat: begin new main class

---
+++ src/main/java/xyz/zuner/javafxassignment/StoreApplication.java
@@ -0,0 +1,23 @@
+package xyz.zuner.javafxassignment;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main class.
+ *
+ * 21:198:102/02: Computers and Programming II
+ * JavaFX Assignment
+ * Rutgers ID - 199009651
+ *
+ * @author Zeyad "zmr15" Rashed
+ * @mailto zmr15@scarletmail.rutgers.edu
+ * @created 3/14/24, Thursday
+ **/
+public class StoreApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
